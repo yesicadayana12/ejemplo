bee321cbb4e4c7ca886ffb331f6031293617e200
Yesica Beltran <guzman.carvajal240406@gmail.com>
2023-10-17 15:38:43 -0500

    hola

---
+++ src/ejem.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.util.jar.JarOutputStream;
+
+public class ejem {
+    public class ciclo {
+     public static void main(String [] args) {
+
+
+            String []rasasp = {"pitbul", "pinche", "shoscie","salchicha","pastor aleman","bulldog"};
+
+            for (String i: rasasp){
+                JOptionPane.showMessageDialog(null,"rasasp " + "\n" + i) ;
+
+
+            }
+
+        }
+    }
+}
